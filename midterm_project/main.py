99385e52f1c072b29f52ddab05d7331f7840be4e
Sargis <sargishovsepyan2001@gmail.com>
2023-12-21 20:05:54 +0400

    [ADD] Creating binary images

--- midterm_project/main.py
+++ midterm_project/main.py
@@ -1,18 +1,11 @@
-from process.feature_selection import *
+from pre_processing import *
 
-# Example usage
-path = "cs_handwriting_bin\H102\OOP.MT1.170215.H102_p2_bin.jpg"
+# Creating binary images of handwritings from cropped versions
+input_folders = ["cs_handwriting_cropped/H101", "cs_handwriting_cropped/H102"]
+output_folders = ["cs_handwriting_bin/H101", "cs_handwriting_bin/H102"]
 
-props = evaluate_page_properties(path)
-font_size = evaluate_font_size(path)
-baseline = evaluate_baseline_orientation(path)
-slant = evaluate_letter_slant(path)
-space = evaluate_inter_word_spacing(path)
+os.makedirs(output_folders[0], exist_ok=True)
+os.makedirs(output_folders[1], exist_ok=True)
 
-print("\n")
-print("Page Size:               ", props["size"])
-print("Page Orientation:        ", props["orientation"])
-print("Baseline Orientation:    ", baseline)
-print("Avarage Word Spacing:    ", space)
-print("Avarage Font Size:       ", font_size)
-print("Avarage Line Slant:      ", slant)
+tune_and_convert(input_folders[0], output_folders[0])
+tune_and_convert(input_folders[1], output_folders[1])
